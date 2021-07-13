1bb7da07a2d98a381aec7cf324f60fa668c8199c
Techercise <mjl1997@verizon.net>
2021-07-13 06:36:47 -0700

    Updating Cori job scripts

--- examples/corigpu_1GPU_slurm.cmd
+++ examples/corigpu_1GPU_slurm.cmd
@@ -1,15 +1,18 @@
 #!/bin/bash
 #SBATCH -C gpu
-#SBATCH -t 01:30:00
+#SBATCH -t 03:30:00
 #SBATCH -G 1
-#SBATCH -c 4
-#SBATCH --exclusive
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-socket=1
+#SBATCH -c 1
 
-# rm /global/cscratch1/sd/$USER/model_checkpoints/*
-# rm /global/cscratch1/sd/$USER/results/*
-# rm /global/cscratch1/sd/$USER/csv_logs/*
-# rm /global/cscratch1/sd/$USER/Graph/*
-# rm /global/cscratch1/sd/$USER/normalization/*
+module load cgpu
+module load python
+module load cuda
+module load cudnn
+module load gcc
+module load openmpi
+source activate test
 
-export OMPI_MCA_btl="tcp,self,vader"
-srun python mpi_learn.py
+srun -C gpu -t 04:00:00 -G 1 python mpi_learn.py
--- examples/corigpu_4GPU_slurm.cmd
+++ examples/corigpu_4GPU_slurm.cmd
@@ -2,18 +2,19 @@
 #SBATCH -C gpu
 #SBATCH -t 02:00:00
 #SBATCH -G 4
+#SBATCH -N 1
 #SBATCH --ntasks-per-node=4
 #SBATCH --ntasks-per-socket=2
 #SBATCH -c 4
-#SBATCH --mem-per-cpu=0
+#SBATCH --exclusive
 
 module load cgpu
 module load python
-source activate test
 module load cuda
 module load cudnn
 module load gcc
 module load openmpi
+source activate test
 
 # rm /global/cscratch1/sd/$USER/model_checkpoints/*
 # rm /global/cscratch1/sd/$USER/results/*
@@ -21,4 +22,4 @@ module load openmpi
 # rm /global/cscratch1/sd/$USER/Graph/*
 # rm /global/cscratch1/sd/$USER/normalization/*
 
-mpirun -N 4 python mpi_learn.py
+srun -C gpu -t 03:00:00 -G 4 -N 1 --ntasks-per-node=4 --ntasks-per-socket=2 -c 4 --exclusive  python mpi_learn.py
