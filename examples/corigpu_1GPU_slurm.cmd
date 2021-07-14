de649d8bf86b4268cc218feebd51f61db06aee5a
Techercise <mjl1997@verizon.net>
2021-07-14 07:52:47 -0700

    update job script

--- examples/corigpu_1GPU_slurm.cmd
+++ examples/corigpu_1GPU_slurm.cmd
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -C gpu
-#SBATCH -t 03:30:00
+#SBATCH -t 03:00:00
 #SBATCH -G 1
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
@@ -15,4 +15,4 @@ module load gcc
 module load openmpi
 source activate test
 
-srun -C gpu -t 04:00:00 -G 1 python mpi_learn.py
+srun -C gpu -t 03:00:00 -G 1 python mpi_learn.py
