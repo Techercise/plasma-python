2851e400ca4331f850b50f5d0bca57048fe45e02
Techercise <mjl1997@verizon.net>
2021-07-14 11:46:56 -0700

    Adding profiling batch job

---
+++ examples/corigpu_1GPU_dlprof_slurm.cmd
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -C gpu
+#SBATCH -t 03:00:00
+#SBATCH -G 1
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-socket=1
+#SBATCH -c 1
+
+module load cgpu
+module load python
+module load cuda
+module load cudnn
+module load gcc
+module load openmpi
+source activate test
+
+srun -C gpu -t 03:00:00 -G 1 dlprof python mpi_learn.py
